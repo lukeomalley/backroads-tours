bc0e0179d191b70e27d8a3fc6ffddd64d09bb817
Luke O'Malley <Lukeomalley95@gmail.com>
2019-06-30 17:27:00 -0400

    setup gatsby-source-filesystem and gatsby-image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
+  siteMetadata: {
+    title: `Backroads`,
+    siteUrl: `https://organic-backroads.netlify.com/`,
+    description: `Explore worldwide tours and discover the all the unique corners of the world.`,
+    author: `@lukeomalley`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-export default function Blog() {
+const Blog = ({ data }) => {
   return (
     <Layout>
       <p>Blog Page</p>
     </Layout>
   );
-}
+};
+
+export default Blog;
